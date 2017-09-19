111a2cece4508b8703f132c1ba65c0c87440d569
Joseph Morgan <jmorgan9@yahoo.com>
2017-09-19 05:56:29 -0700

    first commit

---
+++ APP/data/friend.js
@@ -0,0 +1,21 @@
+var friendList = [
+	{
+		name: 'Joseph Morgan',
+		profilePic: '',
+		scores: [
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5",
+			"5"
+		]
+	}
+]
+
+module.exports = friendList;
+//need to finish
